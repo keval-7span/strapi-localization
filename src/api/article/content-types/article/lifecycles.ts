dc204819cd5785c0d656387c619af098097d0c3c
Keval7Span <keval@7span.com>
2025-01-28 13:28:32 +0530

    Initial Setup

---
+++ src/api/article/content-types/article/lifecycles.ts
@@ -0,0 +1,28 @@
+export default {
+    afterCreate: async (event) => {
+        const { result } = event;
+        const { documentId, title, description, locale } = result;
+
+        if (locale === 'en') {
+            const locales = await strapi.documents("plugin::i18n.locale").findMany({
+                fields: ["code"],
+                filters: {
+                    code: {
+                        "$ne": locale
+                    }
+                }
+            });
+
+            for (const nested_locale of locales) {
+                await strapi.documents("api::article.article").update({
+                    documentId,
+                    locale: nested_locale.code,
+                    data: {
+                        title,
+                        description
+                    }
+                });
+            }
+        }
+    }
+}
